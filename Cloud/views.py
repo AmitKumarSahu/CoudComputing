2730235c68ce5311a867e5277c14655dd2a7cdbe
AmitKumarSahu <amitsahu6673@gmail.com>
2018-04-14 12:47:06 +0530

    anothertest

--- Cloud/views.py
+++ Cloud/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<marquee><h1>This is the cloud computing class</h1></marquee>")
+    return render(request, "cloud/index.html")
